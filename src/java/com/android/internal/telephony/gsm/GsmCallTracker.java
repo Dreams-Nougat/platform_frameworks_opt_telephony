d211f581f5f60d870478d0e90cfa5fdac41ed270
Hariprasad Jayakumar <hjayak@codeaurora.org>
2011-11-15 23:37:50 -0800

    Telephony: Multiparty Call related fix
    Block multiple Call switch request to RIL
    
    Sending a call switch request to RIL before receiving
    a response for the previous request causes the lower
    layers to not respond. Pending responses from RIL
    block the call polling in telephony.
    
    Change-Id: I40eafeefa0860fd69969f0f592c54bd7861b2964
    CRs-Fixed: 269330, 278014, 282061, 293727, 441309

--- src/java/com/android/internal/telephony/gsm/GsmCallTracker.java
+++ src/java/com/android/internal/telephony/gsm/GsmCallTracker.java
@@ -88,6 +88,7 @@ public final class GsmCallTracker extends CallTracker {
 
     PhoneConstants.State mState = PhoneConstants.State.IDLE;
 
+    boolean mCallSwitchPending = false;
 
 
     //***** Events
@@ -293,9 +294,12 @@ public final class GsmCallTracker extends CallTracker {
         // Should we bother with this check?
         if (mRingingCall.getState() == GsmCall.State.INCOMING) {
             throw new CallStateException("cannot be in the incoming state");
-        } else {
+        } else if (mCallSwitchPending == false) {
             mCi.switchWaitingOrHoldingAndActive(
                     obtainCompleteMessage(EVENT_SWITCH_RESULT));
+            mCallSwitchPending = true;
+        } else {
+            Log.w(LOG_TAG, "Call Switch request ignored due to pending response");
         }
     }
 
@@ -880,6 +884,7 @@ public final class GsmCallTracker extends CallTracker {
             break;
 
             case EVENT_SWITCH_RESULT:
+                mCallSwitchPending = false;
             case EVENT_CONFERENCE_RESULT:
             case EVENT_SEPARATE_RESULT:
             case EVENT_ECT_RESULT:
