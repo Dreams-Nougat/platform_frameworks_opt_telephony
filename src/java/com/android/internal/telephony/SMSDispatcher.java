4e2f8355e9b8bfe85e66a0a5317d39c952ce7246
Rika Brooks <rbrooks@codeaurora.org>
2012-06-06 17:16:56 -0700

    Telephony: Set TP-RD to 1 in case of retry
    per TS 23.040 Section 9.2.3.6: In the case where no response or an
    RP-ERROR with an appropriate cause value (see 3GPP TS 24.011 [13]) is
    received in response to an SMS-SUBMIT, then the MS shall automatically
    repeat the SMS-SUBMIT but must use the same TP-MR value
    and set the TP-RD bit to 1 (see 9.2.3.25)
    
    Change-Id: I1c7ebc67f80a6eeba456ab0dc5f6331a038d7ab2

--- src/java/com/android/internal/telephony/SMSDispatcher.java
+++ src/java/com/android/internal/telephony/SMSDispatcher.java
@@ -366,6 +366,12 @@ public abstract class SMSDispatcher extends Handler {
         SmsTracker tracker = (SmsTracker) ar.userObj;
         PendingIntent sentIntent = tracker.mSentIntent;
 
+        if (ar.result != null) {
+            tracker.mMessageRef =  ((SmsResponse)ar.result).messageRef;
+        } else {
+            Log.e(TAG, "SmsResponse was null ");
+        }
+
         if (ar.exception == null) {
             if (false) {
                 Log.d(TAG, "SMS send complete. Broadcasting "
@@ -374,8 +380,6 @@ public abstract class SMSDispatcher extends Handler {
 
             if (tracker.mDeliveryIntent != null) {
                 // Expecting a status report.  Add it to the list.
-                int messageRef = ((SmsResponse)ar.result).messageRef;
-                tracker.mMessageRef = messageRef;
                 deliveryPendingList.add(tracker);
             }
 
