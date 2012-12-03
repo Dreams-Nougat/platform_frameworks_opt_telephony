72cb777d9364ac7fc34be71bfb35073ed82597f9
Arun Ravindran <arun.ravindran@intel.com>
2012-12-03 12:12:59 +0200

    Telephony: Enabling Network Personalization PUK in Framework
    Network personalisation is the feature were a SIM is restricted
    to be used in a specific operator networks only. If an unsupported
    SIM is used in the device, the user is asked to enter an unlock
    code, so that the SIM can be used in the network. If the user enters
    wrong PIN for a preprogrammed number of times, the device is PUK
    locked and needs to be taken to the customer care. Android by default
    supports entering the PIN, but doesn't inform the user if the device
    is PUK locked.
    
    This patch handles the NETWORK PUK required status from modem in
    command exception, handles the new event registration and notifications
    in IccCardProxy and UiccCardApplication.
    
    Note: This patch is related to a change in platforms/frameworks/base,
    		platform/frameworks/opt/telephony and platform/packages/apps/Phone
    
    Change-Id: Id3b93f9085e7508770eb232ba50b728ffa4d54e2
    Author: Arun Ravindran <arun.ravindran@intel.com>
    Signed-off-by: Arun Ravindran <arun.ravindran@intel.com>
    Signed-off-by: Jeevaka Badrappan <jeevaka.badrappan@intel.com>
    Signed-off-by: Shuo Gao <shuo.gao@intel.com>
    Signed-off-by: Bruce Beare <bruce.j.beare@intel.com>
    Signed-off-by: Jack Ren <jack.ren@intel.com>
    Author-tracking-BZ: 18871

--- src/java/com/android/internal/telephony/uicc/IccCardProxy.java
+++ src/java/com/android/internal/telephony/uicc/IccCardProxy.java
@@ -83,6 +83,7 @@ public class IccCardProxy extends Handler implements IccCard {
     private static final int EVENT_IMSI_READY = 8;
     private static final int EVENT_NETWORK_LOCKED = 9;
     private static final int EVENT_CDMA_SUBSCRIPTION_SOURCE_CHANGED = 11;
+    private static final int EVENT_NETWORK_LOCKED_PUK = 12;
 
     private final Object mLock = new Object();
     private Context mContext;
@@ -91,6 +92,7 @@ public class IccCardProxy extends Handler implements IccCard {
     private RegistrantList mAbsentRegistrants = new RegistrantList();
     private RegistrantList mPinLockedRegistrants = new RegistrantList();
     private RegistrantList mNetworkLockedRegistrants = new RegistrantList();
+    private RegistrantList mNetworkLockedPukRegistrants = new RegistrantList();
 
     private int mCurrentAppType = UiccController.APP_FAM_3GPP; //default to 3gpp?
     private UiccController mUiccController = null;
@@ -237,6 +239,10 @@ public class IccCardProxy extends Handler implements IccCard {
             case EVENT_CDMA_SUBSCRIPTION_SOURCE_CHANGED:
                 updateQuietMode();
                 break;
+            case EVENT_NETWORK_LOCKED_PUK:
+                mNetworkLockedPukRegistrants.notifyRegistrants();
+                setExternalState(State.NETWORK_LOCKED_PUK);
+                break;
             default:
                 loge("Unhandled message with number: " + msg.what);
                 break;
@@ -300,6 +306,8 @@ public class IccCardProxy extends Handler implements IccCard {
             case APPSTATE_SUBSCRIPTION_PERSO:
                 if (mUiccApplication.getPersoSubState() == PersoSubState.PERSOSUBSTATE_SIM_NETWORK) {
                     setExternalState(State.NETWORK_LOCKED);
+                } else if (mUiccApplication.getPersoSubState().isSubscriptionPukRequired()) {
+                   setExternalState(State.NETWORK_LOCKED_PUK);
                 } else {
                     setExternalState(State.UNKNOWN);
                 }
@@ -316,6 +324,7 @@ public class IccCardProxy extends Handler implements IccCard {
             mUiccApplication.registerForReady(this, EVENT_APP_READY, null);
             mUiccApplication.registerForLocked(this, EVENT_ICC_LOCKED, null);
             mUiccApplication.registerForNetworkLocked(this, EVENT_NETWORK_LOCKED, null);
+            mUiccApplication.registerForNetworkLockedPuk(this, EVENT_NETWORK_LOCKED_PUK, null);
         }
         if (mIccRecords != null) {
             mIccRecords.registerForImsiReady(this, EVENT_IMSI_READY, null);
@@ -328,6 +337,7 @@ public class IccCardProxy extends Handler implements IccCard {
         if (mUiccApplication != null) mUiccApplication.unregisterForReady(this);
         if (mUiccApplication != null) mUiccApplication.unregisterForLocked(this);
         if (mUiccApplication != null) mUiccApplication.unregisterForNetworkLocked(this);
+        if (mUiccApplication != null) mUiccApplication.unregisterForNetworkLockedPuk(this);
         if (mIccRecords != null) mIccRecords.unregisterForImsiReady(this);
         if (mIccRecords != null) mIccRecords.unregisterForRecordsLoaded(this);
     }
@@ -412,6 +422,7 @@ public class IccCardProxy extends Handler implements IccCard {
             case READY: return IccCardConstants.INTENT_VALUE_ICC_READY;
             case NOT_READY: return IccCardConstants.INTENT_VALUE_ICC_NOT_READY;
             case PERM_DISABLED: return IccCardConstants.INTENT_VALUE_ICC_LOCKED;
+            case NETWORK_LOCKED_PUK: return IccCardConstants.INTENT_VALUE_LOCKED_NETWORK_PUK;
             default: return IccCardConstants.INTENT_VALUE_ICC_UNKNOWN;
         }
     }
@@ -498,6 +509,29 @@ public class IccCardProxy extends Handler implements IccCard {
     public void unregisterForNetworkLocked(Handler h) {
         synchronized (mLock) {
             mNetworkLockedRegistrants.remove(h);
+        }
+    }
+
+    /**
+     * Notifies handler of any transition into State.NETWORK_LOCKED_PUK
+     */
+    @Override
+    public void registerForNetworkLockedPuk(Handler h, int what, Object obj) {
+        synchronized (mLock) {
+            Registrant r = new Registrant (h, what, obj);
+
+            mNetworkLockedPukRegistrants.add(r);
+
+            if (getState() == State.NETWORK_LOCKED_PUK) {
+                r.notifyRegistrant();
+            }
+        }
+    }
+
+    @Override
+    public void unregisterForNetworkLockedPuk(Handler h) {
+        synchronized (mLock) {
+            mNetworkLockedPukRegistrants.remove(h);
         }
     }
 
