Jackeagle/caf_frameworks_base

Telephony: Return 'null' when SubscriberId and Line1Number are unavailable
Return 'null' instead of "" (empty string) when Subscriber Id and
Line1Number  are unavailable

Change-Id: Icf0582744f4e05397f7d3721f8a7503102313dab
CRs-Fixed: 350242

telephony/java/com/android/internal/telephony/gsm/GSMPhone.java
@@ -918,11 +918,11 @@ public class GSMPhone extends PhoneBase {
     }
 
     public String getSubscriberId() {
-        return (mIccRecords != null) ? mIccRecords.getIMSI() : "";
+        return (mIccRecords != null) ? mIccRecords.getIMSI() : null;
     }
 
     public String getLine1Number() {
-        return (mIccRecords != null) ? mIccRecords.getMsisdnNumber() : "";
+        return (mIccRecords != null) ? mIccRecords.getMsisdnNumber() : null;
     }
 
     @Override

Dreams-Nougat/platform_frameworks_opt_telephony

src/java/com/android/internal/telephony/gsm/GSMPhone.java
@@ -853,12 +853,12 @@ public class GSMPhone extends PhoneBase {
 
     public String getSubscriberId() {
         IccRecords r = mIccRecords.get();
-        return (r != null) ? r.getIMSI() : "";
+        return (r != null) ? r.getIMSI() : null;
     }
 
     public String getLine1Number() {
         IccRecords r = mIccRecords.get();
-        return (r != null) ? r.getMsisdnNumber() : "";
+        return (r != null) ? r.getMsisdnNumber() : null;
     }
 
     @Override