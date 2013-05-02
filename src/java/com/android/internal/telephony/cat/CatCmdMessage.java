e930b204ed853c959b6f6b8f0251b43d46419254
Sandeep Kunta <skunta@codeaurora.org>
2013-05-02 15:57:51 +0530

    Stk: Add support for alpha in REFRESH command
    RIL_UNSOL_STK_EVENT_NOTIFY is given by RIL in case there is alpha
    id to be displayed to user in refresh proactive cmd.
    Support is added to display Alpha id to user when this event comes
    due to REFRESH proactive cmd.
    Also if refresh mode is init or reset, idle mode text needs to be
    cleared as per stk 3GPP spec 11.14. This logic is handled in stk
    app to satisfy both the above requiremnts.
    
    Change-Id: Ic16da08d611753f2ee4a5b288f46b985078ca46f

--- src/java/com/android/internal/telephony/cat/CatCmdMessage.java
+++ src/java/com/android/internal/telephony/cat/CatCmdMessage.java
@@ -34,6 +34,11 @@ public class CatCmdMessage implements Parcelable {
     private ToneSettings mToneSettings = null;
     private CallSettings mCallSettings = null;
 
+    // Command Qualifier values for refresh command
+    static final int REFRESH_NAA_INIT_AND_FULL_FILE_CHANGE  = 0x00;
+    static final int REFRESH_NAA_INIT_AND_FILE_CHANGE       = 0x02;
+    static final int REFRESH_NAA_INIT                       = 0x03;
+    static final int REFRESH_UICC_RESET                     = 0x04;
     /*
      * Container for Launch Browser command settings.
      */
@@ -61,6 +66,7 @@ public class CatCmdMessage implements Parcelable {
         case SET_UP_IDLE_MODE_TEXT:
         case SEND_DTMF:
         case SEND_SMS:
+        case REFRESH:
         case SEND_SS:
         case SEND_USSD:
             mTextMsg = ((DisplayTextParams) cmdParams).mTextMsg;
@@ -96,7 +102,6 @@ public class CatCmdMessage implements Parcelable {
             mTextMsg = param.mTextMsg;
             break;
         case PROVIDE_LOCAL_INFORMATION:
-        case REFRESH:
         case SET_UP_EVENT_LIST:
         default:
             break;
@@ -194,5 +199,16 @@ public class CatCmdMessage implements Parcelable {
 
     public CallSettings getCallSettings() {
         return mCallSettings;
+    }
+
+    public boolean isRefreshResetOrInit() {
+        if ((mCmdDet.commandQualifier == REFRESH_NAA_INIT_AND_FULL_FILE_CHANGE)
+            || (mCmdDet.commandQualifier == REFRESH_NAA_INIT_AND_FILE_CHANGE )
+            || (mCmdDet.commandQualifier == REFRESH_NAA_INIT)
+            || (mCmdDet.commandQualifier == REFRESH_UICC_RESET)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
