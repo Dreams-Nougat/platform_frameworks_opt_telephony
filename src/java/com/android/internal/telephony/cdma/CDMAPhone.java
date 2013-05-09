7468499fd1ae5683b068708b0870897a32df81bc
Abhishek Adappa <aadappa@codeaurora.org>
2013-05-09 13:13:09 -0700

    IccPhoneBookInterfaceManager will decide which Card App to use.
    Phone will not tell IccPhoneBoookIntefaceManager which record
    to use. Phone will only provide the card associated with it.
    
    PhoneBook will iterate through the apps on the card to
    determine if it is a 3G or 2G card and use EF_PBR or
    EF_ADN respectively.
    
    Change-Id: Icf5997dd6a6ba5e1de675cf5f4334c78c2c037f1
    CRs-Fixed: 478627

--- src/java/com/android/internal/telephony/cdma/CDMAPhone.java
+++ src/java/com/android/internal/telephony/cdma/CDMAPhone.java
@@ -1184,11 +1184,25 @@ public class CDMAPhone extends PhoneBase {
         return  mUiccController.getUiccCardApplication(UiccController.APP_FAM_3GPP2);
     }
 
+    // Set the Card into the Phone Book.
+    @Override
+    protected void setCardInPhoneBook() {
+        if (mUiccController == null ) {
+            return;
+        }
+
+        mRuimPhoneBookInterfaceManager.setIccCard(mUiccController.getUiccCard());
+    }
+
     @Override
     protected void onUpdateIccAvailability() {
         if (mUiccController == null ) {
             return;
         }
+
+        // Get the latest info on the card and
+        // send this to Phone Book
+        setCardInPhoneBook();
 
         UiccCardApplication newUiccApplication = getUiccCardApplication();
 
@@ -1204,7 +1218,6 @@ public class CDMAPhone extends PhoneBase {
                 log("Removing stale icc objects.");
                 if (mIccRecords.get() != null) {
                     unregisterForRuimRecordEvents();
-                    mRuimPhoneBookInterfaceManager.updateIccRecords(null);
                 }
                 mIccRecords.set(null);
                 mUiccApplication.set(null);
@@ -1214,7 +1227,6 @@ public class CDMAPhone extends PhoneBase {
                 mUiccApplication.set(newUiccApplication);
                 mIccRecords.set(newUiccApplication.getIccRecords());
                 registerForRuimRecordEvents();
-                mRuimPhoneBookInterfaceManager.updateIccRecords(mIccRecords.get());
             }
         }
     }
