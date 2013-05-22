32dd5703ee4d65f2a0098404f214dfa08f073427
Sandeep Kunta <skunta@codeaurora.org>
2013-05-22 18:04:56 +0530

    Stk: dispose objects properly for hotswap
    During hotswap, CatService object is disposed and new
    object is created. However RilMessageDecoder still
    holds reference to old object of CatService, and hence sends
    decoded command to old object of CatService leading to stk failure.
    Same issue comes during airplane mode on/off also, if card is
    powered down in airplane mode.
    In this fix below objects are disposed properly when CatService
    is disposed.
    
    1) RilMessageDecoder
    2) CommandParamsFactory
    3) IconLoader
    
    Change-Id: I60db989fd001012198fc4806465bb03d55a4ffbe

--- src/java/com/android/internal/telephony/cat/CatService.java
+++ src/java/com/android/internal/telephony/cat/CatService.java
@@ -72,6 +72,7 @@ public class CatService extends Handler implements AppInterface {
     // Protects singleton instance lazy initialization.
     private static final Object sInstanceLock = new Object();
     private static CatService sInstance;
+    protected static HandlerThread mHandlerThread;
     private CommandsInterface mCmdIf;
     private Context mContext;
     private CatCmdMessage mCurrntCmd = null;
@@ -136,13 +137,18 @@ public class CatService extends Handler implements AppInterface {
     }
 
     public void dispose() {
+        CatLog.d(this, "Disposing CatService object");
         mIccRecords.unregisterForRecordsLoaded(this);
         mCmdIf.unSetOnCatSessionEnd(this);
         mCmdIf.unSetOnCatProactiveCmd(this);
         mCmdIf.unSetOnCatEvent(this);
         mCmdIf.unSetOnCatCallSetUp(this);
-
+        mMsgDecoder.dispose();
+        mMsgDecoder = null;
+        mHandlerThread.quit();
+        mHandlerThread = null;
         removeCallbacksAndMessages(null);
+        sInstance = null;
     }
 
     @Override
@@ -535,8 +541,8 @@ public class CatService extends Handler implements AppInterface {
                         || ic == null) {
                     return null;
                 }
-                HandlerThread thread = new HandlerThread("Cat Telephony service");
-                thread.start();
+                mHandlerThread = new HandlerThread("Cat Telephony service");
+                mHandlerThread.start();
                 sInstance = new CatService(ci, ca, ir, context, fh, ic);
                 CatLog.d(sInstance, "NEW sInstance");
             } else if ((ir != null) && (mIccRecords != ir)) {
