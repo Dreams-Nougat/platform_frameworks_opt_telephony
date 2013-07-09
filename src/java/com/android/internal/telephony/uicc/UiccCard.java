9b4ef69d19d0c3335fe70ba431ec4848525188b3
Abhishek Adappa <aadappa@codeaurora.org>
2013-07-08 17:59:23 -0700

    Clean up Icc Refresh handling.
    1. When a SIM refresh is received of type INIT or RESET,
    don't immediately start re-fetching the records, because
    the app might not be ready. In this case, we could end up
    broadcasting that all records are loaded pre-maturely.
    
    On a refresh of the above type, the app will move out of the
    ready state and back into the PIN state, if PIN is enabled.
    Start reading the records only after the PIN has been verified
    and the app has moved to the READY state.
    
    2. UiccController will listen for refresh and propagate it to
    the Card and the Apps. The App will set its state to UNKNOWN
    on receiving a refresh.
    UiccController will also request card status on receiving
    refresh to get the latest state.
    
    This avoids having to make assumptions on the ordering of the
    Unsols for Sim Refresh and card state changes. This also
    ensures that after an app state is marked UNKNOWN,
    it will move to READY, if it is truly ready.
    
    3. Moved common implementation for refresh into IccRecords
    for better code reuse.
    
    4. Side effect of #3 is RuimRecords will now reset AdnCache when
    refresh of type INIT is received.
    
    Change-Id: I58b1237a49d4f943a699d751a8680c25ce4394ad

--- src/java/com/android/internal/telephony/uicc/UiccCard.java
+++ src/java/com/android/internal/telephony/uicc/UiccCard.java
@@ -347,6 +347,26 @@ public class UiccCard {
         }
     }
 
+    /* Returns number of applications on this card */
+    public int getNumApplications() {
+        int count = 0;
+        for (UiccCardApplication a : mUiccApplications) {
+            if (a != null) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    void onRefresh(IccRefreshResponse refreshResponse){
+        for ( int i = 0; i < mUiccApplications.length; i++) {
+            if (mUiccApplications[i] != null) {
+                // Let the app know that the refresh occurred
+                mUiccApplications[i].onRefresh(refreshResponse);
+            }
+        }
+    }
+
     private void log(String msg) {
         Rlog.d(LOG_TAG, msg);
     }
