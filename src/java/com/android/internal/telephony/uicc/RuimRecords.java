lt03lte-cafaosp/platform-frameworks-opt-telephony

Telephony: Display of Operator name.
Operator name is displaying in junk characters on CDMA network
due to wrong encoding scheme.Correct the same.

Change-Id: I4322126acf3bc900b47964f3967730151b8461b7

src/java/com/android/internal/telephony/uicc/RuimRecords.java
@@ -290,9 +290,11 @@ public final class RuimRecords extends IccRecords {
                     break;
                 case UserData.ENCODING_IA5:
                 case UserData.ENCODING_GSM_7BIT_ALPHABET:
-                case UserData.ENCODING_7BIT_ASCII:
                     spn = GsmAlphabet.gsm7BitPackedToString(spnData, 0, (numBytes*8)/7);
                     break;
+                case UserData.ENCODING_7BIT_ASCII:
+                    spn =  new String(spnData, 0, numBytes, "US-ASCII");
+                break;
                 case UserData.ENCODING_UNICODE_16:
                     spn =  new String(spnData, 0, numBytes, "utf-16");
                     break;

Dreams-Nougat/platform_frameworks_opt_telephony

src/java/com/android/internal/telephony/uicc/RuimRecords.java
@@ -305,9 +305,11 @@ public final class RuimRecords extends IccRecords {
                     break;
                 case UserData.ENCODING_IA5:
                 case UserData.ENCODING_GSM_7BIT_ALPHABET:
-                case UserData.ENCODING_7BIT_ASCII:
                     mSpn = GsmAlphabet.gsm7BitPackedToString(spnData, 0, (numBytes*8)/7);
                     break;
+                case UserData.ENCODING_7BIT_ASCII:
+                    mSpn =  new String(spnData, 0, numBytes, "US-ASCII");
+                break;
                 case UserData.ENCODING_UNICODE_16:
                     mSpn =  new String(spnData, 0, numBytes, "utf-16");
                     break;