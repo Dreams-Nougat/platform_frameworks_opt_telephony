lt03lte-cafaosp/platform-frameworks-opt-telephony

Telephony: Multiparty Call related fix
Block multiple Call switch request to RIL

Sending a call switch request to RIL before receiving
a response for the previous request causes the lower
layers to not respond. Pending responses from RIL
block the call polling in telephony.

Change-Id: I40eafeefa0860fd69969f0f592c54bd7861b2964
CRs-Fixed: 269330, 278014, 282061, 293727, 441309

src/java/com/android/internal/telephony/gsm/GsmCallTracker.java
@@ -81,6 +81,8 @@ public final class GsmCallTracker extends CallTracker {
     GsmConnection pendingMO;
     boolean hangupPendingMO;
 
+    boolean callSwitchPending = false;
+
     GSMPhone phone;
 
     boolean desiredMute = false;    // false = mute off
@@ -287,9 +289,12 @@ public final class GsmCallTracker extends CallTracker {
         // Should we bother with this check?
         if (ringingCall.getState() == GsmCall.State.INCOMING) {
             throw new CallStateException("cannot be in the incoming state");
-        } else {
+        } else if (callSwitchPending == false) {
             cm.switchWaitingOrHoldingAndActive(
                     obtainCompleteMessage(EVENT_SWITCH_RESULT));
+            callSwitchPending = true;
+        } else {
+            Log.w(LOG_TAG, "Call Switch request ignored due to pending response");
         }
     }
 
@@ -873,6 +878,7 @@ public final class GsmCallTracker extends CallTracker {
             break;
 
             case EVENT_SWITCH_RESULT:
+                callSwitchPending = false;
             case EVENT_CONFERENCE_RESULT:
             case EVENT_SEPARATE_RESULT:
             case EVENT_ECT_RESULT:

Dreams-Nougat/platform_frameworks_opt_telephony

Change-Id: I078945f7427fdc7b2b26d6563bc117a3dd4fc9c4

src/java/com/android/internal/telephony/gsm/GsmCallTracker.java
@@ -81,6 +81,8 @@ public final class GsmCallTracker extends CallTracker {
     GsmConnection pendingMO;
     boolean mHangupPendingMO;
 
+    boolean callSwitchPending = false;
+
     GSMPhone mPhone;
 
     boolean mDesiredMute = false;    // false = mute off
@@ -282,9 +284,12 @@ public final class GsmCallTracker extends CallTracker {
         // Should we bother with this check?
         if (mRingingCall.getState() == GsmCall.State.INCOMING) {
             throw new CallStateException("cannot be in the incoming state");
-        } else {
+        } else if (callSwitchPending == false) {
             mCi.switchWaitingOrHoldingAndActive(
                     obtainCompleteMessage(EVENT_SWITCH_RESULT));
+            callSwitchPending = true;
+        } else {
+            Rlog.w(LOG_TAG, "Call Switch request ignored due to pending response");
         }
     }
 
@@ -864,6 +869,7 @@ public final class GsmCallTracker extends CallTracker {
             break;
 
             case EVENT_SWITCH_RESULT:
+                callSwitchPending = false;
             case EVENT_CONFERENCE_RESULT:
             case EVENT_SEPARATE_RESULT:
             case EVENT_ECT_RESULT: