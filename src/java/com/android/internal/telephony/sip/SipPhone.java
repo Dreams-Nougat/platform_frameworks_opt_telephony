lt03lte-cafaosp/platform-frameworks-opt-telephony

Do not set audioGroup ECHO SUPRESSION mode
when speaker enable, do not set AudioGroup EC mode
if EC already supported in hardware.

Change-Id: I66536de58a52e027756817b8c7f9007e4ee51d05

src/java/com/android/internal/telephony/sip/SipPhone.java
@@ -300,7 +300,10 @@ public class SipPhone extends SipPhoneBase {
         // to something like onSpeaerphoneStateChanged(). Echo suppression may
         // not be available on every device.
         synchronized (SipPhone.class) {
-            foregroundCall.setAudioGroupMode();
+            AudioManager audioManager = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
+            String echoSuppression = audioManager.getParameters("ec_supported");
+            if (echoSuppression.contains("off"))
+                foregroundCall.setAudioGroupMode();
         }
     }
 

Dreams-Nougat/platform_frameworks_opt_telephony

Change-Id: I3623b0c49011d6db5e0060999d17963a7e58f775

src/java/com/android/internal/telephony/sip/SipPhone.java
@@ -337,7 +337,11 @@ public class SipPhone extends SipPhoneBase {
         // to something like onSpeaerphoneStateChanged(). Echo suppression may
         // not be available on every device.
         synchronized (SipPhone.class) {
-            mForegroundCall.setAudioGroupMode();
+            AudioManager audioManager = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
+            String echoSuppression = audioManager.getParameters("ec_supported");
+            if (echoSuppression.contains("off")) {
+                mForegroundCall.setAudioGroupMode();
+            }
         }
     }
 