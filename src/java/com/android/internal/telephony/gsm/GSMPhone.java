gp-b2g/frameworks_base

Telephony:Prevent duplicate auto mode request on EF_CSP read
Prevent duplicate auto network selection mode request
if triggered internally by Android due to
PLMN disabled in EF_CSP SIM record.
Allow mode change request if triggered by user.

Change-Id: Ib3f4d807772d33429f8e1d899a12b2684afc52a2
CRs-Fixed: 301052

telephony/java/com/android/internal/telephony/gsm/GSMPhone.java
@@ -1295,8 +1295,15 @@ public class GSMPhone extends PhoneBase {
                 break;
 
             case EVENT_SET_NETWORK_AUTOMATIC:
-                ar = (AsyncResult)msg.obj;
-                setNetworkSelectionModeAutomatic((Message)ar.result);
+                // Automatic network selection from EF_CSP SIM record
+                ar = (AsyncResult) msg.obj;
+                if (mSST.ss.getIsManualSelection()) {
+                    setNetworkSelectionModeAutomatic((Message) ar.result);
+                } else {
+                    // prevent duplicate request which will push current PLMN to
+                    // low priority
+                    Log.d(LOG_TAG, "Stop duplicate SET_NETWORK_SELECTION_AUTOMATIC to Ril ");
+                }
                 break;
 
             case EVENT_ICC_RECORD_EVENTS:

Dreams-Nougat/platform_frameworks_opt_telephony

Change-Id: I9f6828e59abada64fab66d6ff747bf83a3ec4d6c

src/java/com/android/internal/telephony/gsm/GSMPhone.java
@@ -1355,8 +1355,15 @@ public class GSMPhone extends PhoneBase {
                 break;
 
             case EVENT_SET_NETWORK_AUTOMATIC:
-                ar = (AsyncResult)msg.obj;
-                setNetworkSelectionModeAutomatic((Message)ar.result);
+                // Automatic network selection from EF_CSP SIM record
+                ar = (AsyncResult) msg.obj;
+                if (mSST.mSS.getIsManualSelection()) {
+                    setNetworkSelectionModeAutomatic((Message) ar.result);
+                } else {
+                    // prevent duplicate request which will push current PLMN to
+                    // low priority
+                    Rlog.d(LOG_TAG, "Stop duplicate SET_NETWORK_SELECTION_AUTOMATIC to Ril ");
+                }
                 break;
 
             case EVENT_ICC_RECORD_EVENTS: