faa70e9b70513f09ae93811c6e43f9f8928f4f89
Sandeep Gutta <sangutta@codeaurora.org>
2014-02-18 00:21:57 +0530

    WIP Move MSIM handling to base files
    Change-Id: I6484a39d6eb1cfd176f84ecb84200a23dd71a360

--- src/java/com/android/internal/telephony/gsm/GsmSMSDispatcher.java
+++ src/java/com/android/internal/telephony/gsm/GsmSMSDispatcher.java
@@ -25,6 +25,7 @@ import android.os.Message;
 import android.provider.Telephony.Sms;
 import android.provider.Telephony.Sms.Intents;
 import android.telephony.Rlog;
+import android.telephony.TelephonyManager;
 
 import com.android.internal.telephony.GsmAlphabet;
 import com.android.internal.telephony.ImsSMSDispatcher;
@@ -35,6 +36,7 @@ import com.android.internal.telephony.SmsConstants;
 import com.android.internal.telephony.SmsHeader;
 import com.android.internal.telephony.SmsStorageMonitor;
 import com.android.internal.telephony.SmsUsageMonitor;
+import com.android.internal.telephony.SubscriptionManager;
 import com.android.internal.telephony.TelephonyProperties;
 import com.android.internal.telephony.uicc.IccRecords;
 import com.android.internal.telephony.uicc.IccUtils;
@@ -264,7 +266,19 @@ public final class GsmSMSDispatcher extends SMSDispatcher {
     }
 
     protected UiccCardApplication getUiccCardApplication() {
-        return mUiccController.getUiccCardApplication(UiccController.APP_FAM_3GPP);
+        SubscriptionManager subMgr = SubscriptionManager.getInstance();
+        if (subMgr != null) {
+            Rlog.d(TAG, "GsmSMSDispatcher: subId = " + mPhone.getSubscription()
+                    + " slotId = " + mPhone.getSubscription());
+            if (TelephonyManager.getDefault().isMultiSimEnabled()) {
+                return  ((UiccController) mUiccController).getUiccCardApplication(
+                        SubscriptionManager.getInstance().getSlotId(mPhone.getSubscription()),
+                        UiccController.APP_FAM_3GPP);
+            } else {
+                 return  mUiccController.getUiccCardApplication(UiccController.APP_FAM_3GPP);
+            }
+        }
+        return null;
     }
 
     private void onUpdateIccAvailability() {
