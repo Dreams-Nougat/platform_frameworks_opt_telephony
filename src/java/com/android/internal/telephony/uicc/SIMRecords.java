c181818b8a60bdf74356396aa78a1110a719e19a
Ulf Andersson <ulf4.andersson@sonymobile.com>
2015-04-16 10:25:49 +0200

    Always display CPHS Operator Name when not roaming
    According to CPHS spec (section B 4.1.1) the operator name
    should be displayed when in HPLMN, otherwise only current
    PLMN should be used. Adjust SPN display condition accordingly.
    
    Change-Id: Ib760acc3a36a4a5b16c957cbca4397953ce5e4ca

--- src/java/com/android/internal/telephony/uicc/SIMRecords.java
+++ src/java/com/android/internal/telephony/uicc/SIMRecords.java
@@ -1762,6 +1762,8 @@ public class SIMRecords extends IccRecords {
                 if (ar != null && ar.exception == null) {
                     data = (byte[]) ar.result;
                     setServiceProviderName(IccUtils.adnStringFieldToString(data, 0, data.length));
+                    // Display CPHS Operator Name only when not roaming
+                    mSpnDisplayCondition = 2;
 
                     if (DBG) log("Load EF_SPN_CPHS: " + getServiceProviderName());
                     mTelephonyManager.setSimOperatorNameForPhone(
@@ -1780,6 +1782,8 @@ public class SIMRecords extends IccRecords {
                 if (ar != null && ar.exception == null) {
                     data = (byte[]) ar.result;
                     setServiceProviderName(IccUtils.adnStringFieldToString(data, 0, data.length));
+                    // Display CPHS Operator Name only when not roaming
+                    mSpnDisplayCondition = 2;
 
                     if (DBG) log("Load EF_SPN_SHORT_CPHS: " + getServiceProviderName());
                     mTelephonyManager.setSimOperatorNameForPhone(
