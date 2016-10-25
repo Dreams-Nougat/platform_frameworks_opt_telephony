7a612cf17af1571155386b35b0f2b4ace0a592e8
Nathan Harold <nharold@google.com>
2016-10-24 20:05:25 -0700

    Lock the DataStallAlarm against concurrent modification
    Bug: 30877127
    Test: none
    Change-Id: I77403ebdbb97ef8086dadefbbed35a56f94c63f9

--- src/java/com/android/internal/telephony/dataconnection/DcTracker.java
+++ src/java/com/android/internal/telephony/dataconnection/DcTracker.java
@@ -572,6 +572,8 @@ public class DcTracker extends Handler {
     private int mDataStallAlarmTag = (int) SystemClock.elapsedRealtime();
     // The current data stall alarm intent
     private PendingIntent mDataStallAlarmIntent = null;
+    // Synchronization object for the DataStallAlarmIntent;
+    private final Object mDataStallAlarmLock = new Object();
     // Number of packets sent since the last received packet
     private long mSentSinceLastRecv;
     // Controls when a simple recovery attempt it to be tried
@@ -4792,10 +4794,15 @@ public class DcTracker extends Handler {
             }
             Intent intent = new Intent(INTENT_DATA_STALL_ALARM);
             intent.putExtra(DATA_STALL_ALARM_TAG_EXTRA, mDataStallAlarmTag);
-            mDataStallAlarmIntent = PendingIntent.getBroadcast(mPhone.getContext(), 0, intent,
-                    PendingIntent.FLAG_UPDATE_CURRENT);
-            mAlarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP,
-                    SystemClock.elapsedRealtime() + delayInMs, mDataStallAlarmIntent);
+            synchronized(mDataStallAlarmLock) {
+                if(mDataStallAlarmIntent != null) {
+                    Rlog.e(LOG_TAG, "Starting a new data stall alarm before previous is finished!");
+                }
+                mDataStallAlarmIntent = PendingIntent.getBroadcast(mPhone.getContext(), 0, intent,
+                        PendingIntent.FLAG_UPDATE_CURRENT);
+                mAlarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                        SystemClock.elapsedRealtime() + delayInMs, mDataStallAlarmIntent);
+            }
         } else {
             if (VDBG_STALL) {
                 log("startDataStallAlarm: NOT started, no connection tag=" + mDataStallAlarmTag);
@@ -4804,14 +4811,20 @@ public class DcTracker extends Handler {
     }
 
     private void stopDataStallAlarm() {
-        if (VDBG_STALL) {
-            log("stopDataStallAlarm: current tag=" + mDataStallAlarmTag +
-                    " mDataStallAlarmIntent=" + mDataStallAlarmIntent);
-        }
-        mDataStallAlarmTag += 1;
-        if (mDataStallAlarmIntent != null) {
-            mAlarmManager.cancel(mDataStallAlarmIntent);
-            mDataStallAlarmIntent = null;
+        synchronized(mDataStallAlarmLock) {
+            if(mDataStallAlarmIntent == null) {
+                Rlog.e(LOG_TAG, "Attempting to stop a non-existent data stall alarm!");
+                return;
+            }
+            if (VDBG_STALL) {
+                log("stopDataStallAlarm: current tag=" + mDataStallAlarmTag +
+                        " mDataStallAlarmIntent=" + mDataStallAlarmIntent);
+            }
+            mDataStallAlarmTag += 1;
+            if (mDataStallAlarmIntent != null) {
+                mAlarmManager.cancel(mDataStallAlarmIntent);
+                mDataStallAlarmIntent = null;
+            }
         }
     }
 
