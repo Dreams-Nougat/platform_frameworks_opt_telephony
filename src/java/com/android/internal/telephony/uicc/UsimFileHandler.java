695ceff31a717b6945d144b31098b3a7e39485eb
Nathan Harold <nharold@google.com>
2016-10-31 16:27:34 -0700

    WIP Add PLMN Selection Info to SIM Records
    Bug: 32277497
    Test: none
    Change-Id: I28bd67e3667d5c6ba1046c43f24d8e02ce7b8313

--- src/java/com/android/internal/telephony/uicc/UsimFileHandler.java
+++ src/java/com/android/internal/telephony/uicc/UsimFileHandler.java
@@ -63,6 +63,9 @@ public final class UsimFileHandler extends IccFileHandler implements IccConstant
         case EF_GID1:
         case EF_GID2:
         case EF_LI:
+        case EF_PLMN_W_ACT:
+        case EF_OPLMN_W_ACT:
+        case EF_HPLMN_W_ACT:
             return MF_SIM + DF_ADF;
 
         case EF_PBR:
